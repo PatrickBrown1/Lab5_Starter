6b791e8a8aabd58e8af5bc50b7c33c61472b24f6
PatrickBrown1 <patrickbrown1312@gmail.com>
2021-10-21 16:06:31 -0700

    images change when dropdown changes

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,32 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const soundDropdown = document.getElementById("horn-select");
+
+  console.log(soundDropdown);
+
+  soundDropdown.addEventListener('change', (e) => handleSoundUpdate(e.target.value));
+}
+
+
+function handleSoundUpdate (newSound) {
+  switch(newSound) {
+    case "air-horn":
+      updateImageSource("assets/images/air-horn.svg");
+      break;
+    case "car-horn":
+      updateImageSource("assets/images/car-horn.svg");
+
+      break;
+    case "party-horn":
+      updateImageSource("assets/images/party-horn.svg");
+
+      break;
+    default:
+
+  }
+}
+function updateImageSource(newSource) {
+  const image = document.querySelector("#expose > img");
+  image.src = newSource;
 }
